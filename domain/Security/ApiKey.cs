a312255f5c9526a5ea3165d63666de8ebb3243fc
Felipe F Quintella <felipe@quintella.com>
2018-12-17 15:32:51 -0200

    INC

---
+++ domain/Security/ApiKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace adrapi.domain.Security
+{
+	public class ApiKey
+	{
+		public string secretKey { get; set; }
+		public string keyID { get; set; }
+		public string authorizedIP { get; set; }
+		public List<string> claims { get; set; }
+
+		public ApiKey()
+		{
+		}
+	}
+}
