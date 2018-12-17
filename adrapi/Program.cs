d0465aa8fd18e7a9ef07e85f17ac0e3e44c72915
Felipe F Quintella <felipe@quintella.com>
2018-12-17 14:40:34 -0200

    INC

--- adrapi/Program.cs
+++ adrapi/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
 namespace adrapi
 {
@@ -14,7 +16,25 @@ namespace adrapi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -22,9 +42,9 @@ namespace adrapi
                     .ConfigureLogging(logging =>
                     {
                         logging.ClearProviders();
-                        logging.AddConsole();
-                        logging.AddDebug();
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
                     })
+                    .UseNLog()
                     .UseStartup<Startup>();
     }
 }
