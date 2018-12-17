1a70cc0fce4a4dd632e9d4bbbf5d70a8ec1d97c5
Felipe F Quintella <felipe@quintella.com>
2018-12-17 14:15:49 -0200

    INC

--- adrapi/Program.cs
+++ adrapi/Program.cs
@@ -19,6 +19,12 @@ namespace adrapi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddConsole();
+                        logging.AddDebug();
+                    })
+                    .UseStartup<Startup>();
     }
 }
