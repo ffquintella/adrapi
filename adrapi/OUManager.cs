b4cc54c9d850c953026719bff6e2e8a6e99767ee
Felipe F Quintella <felipe@quintella.com>
2018-12-26 14:42:32 -0200

    INC

--- adrapi/OUManager.cs
+++ adrapi/OUManager.cs
@@ -48,5 +48,39 @@ namespace adrapi
             return ous;
         }
 
+        /// <summary>
+        /// Gets the OU.
+        /// </summary>
+        /// <returns>The OU.</returns>
+        /// <param name="DN">The Disitnguesh name of the OU</param>
+        public OU GetOU(string DN)
+        {
+            var sMgmt = LdapSearchManager.Instance;
+
+            var entry = sMgmt.GetRegister(DN);
+
+            var ou = ConvertfromLdap(entry);
+            return ou;
+        }
+
+        /// <summary>
+        /// Converts the data from the LDAP result
+        /// </summary>
+        /// <returns>The LDAP.</returns>
+        /// <param name="entry">Entry.</param>
+        private OU ConvertfromLdap(LdapEntry entry)
+        {
+            var ou = new OU();
+
+            ou.Name = entry.GetAttribute("name").StringValue;
+
+            if (entry.GetAttribute("description") != null) ou.Description = entry.GetAttribute("description").StringValue;
+
+            ou.DN = entry.GetAttribute("distinguishedName").StringValue;
+
+
+            return ou;
+        }
+
     }
 }
