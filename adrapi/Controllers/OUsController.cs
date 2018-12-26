b6f47478882fa9eaa4b400f9d0c66c26f73c897b
Felipe F Quintella <felipe@quintella.com>
2018-12-26 12:38:45 -0200

    INC

--- adrapi/Controllers/OUsController.cs
+++ adrapi/Controllers/OUsController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using adrapi.Web;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using static adrapi.domain.LoggingEvents;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,13 +18,38 @@ namespace adrapi.Controllers
     [ApiVersion("1.0")]
     [Route("api/[controller]")]
     [ApiController]
-    public class OUController : BaseController
+    public class OUsController : BaseController
     {
+
+        public OUsController(ILogger<GroupsController> logger, IConfiguration iConfig)
+        {
+
+            this._logger = logger;
+
+            configuration = iConfig;
+        }
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<String>> Get([FromQuery]int _start, [FromQuery]int _end)
         {
-            return new string[] { "value1", "value2" };
+
+            this.ProcessRequest();
+
+            _logger.LogInformation(GetItem, "{1} listing all ous", requesterID);
+
+            var oManager = OUManager.Instance;
+
+            if (_start == 0 && _end != 0)
+            {
+                return Conflict();
+            }
+
+            //if (_start == 0 && _end == 0)
+                return oManager.GetList();
+            //else return gManager.GetList(_start, _end);
+
+
         }
 
 
--- domain/OU.cs
+++ domain/OU.cs
@@ -3,6 +3,10 @@ namespace adrapi.domain
 {
     public class OU
     {
+
+        public string Name { get; set; }
+        public string DN { get; set; }
+
         public OU()
         {
         }
