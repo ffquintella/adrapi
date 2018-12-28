bddf61460e1ba65bfe3c21eab43fef1875a6ef9c
Felipe F Quintella <felipe@quintella.com>
2018-12-28 11:06:19 -0200

    INC

---
+++ adrapi/Tools/ByteTools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace adrapi.Tools
+{
+    public static class ByteTools
+    {
+        public static bool Equality(byte[] a1, byte[] b1)
+        {
+            int i;
+            if (a1.Length == b1.Length)
+            {
+                i = 0;
+                while (i < a1.Length && (a1[i] == b1[i])) //Earlier it was a1[i]!=b1[i]
+                {
+                    i++;
+                }
+                if (i == a1.Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
