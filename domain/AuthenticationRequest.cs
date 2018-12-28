854fc14440ddd4188b19f53834868d320331720d
Felipe F Quintella <felipe@quintella.com>
2018-12-28 12:39:21 -0200

    INC

---
+++ domain/AuthenticationRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace adrapi.domain
+{
+    public class AuthenticationRequest
+    {
+
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
