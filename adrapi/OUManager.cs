06f4ef773119714ed31db58c294a5c3446a97a92
Felipe F Quintella <felipe@quintella.com>
2019-01-09 12:28:26 -0200

    INC

--- adrapi/OUManager.cs
+++ adrapi/OUManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using adrapi.Ldap;
 using adrapi.domain;
 using Novell.Directory.Ldap;
+using NLog;
+using adrapi.Tools;
 
 namespace adrapi
 {
@@ -57,10 +59,19 @@ namespace adrapi
         {
             var sMgmt = LdapQueryManager.Instance;
 
-            var entry = sMgmt.GetRegister(DN);
+            try
+            {
+                var entry = sMgmt.GetRegister(DN);
+                var ou = ConvertfromLdap(entry); ;
+                return ou;
+            }
+            catch (LdapException ex)
+            {
+                logger.Debug("User not found {0} Ex: {1}", DN, ex.Message);
+                return null;
+            }
 
-            var ou = ConvertfromLdap(entry);
-            return ou;
+
         }
 
         /// <summary>
@@ -80,6 +91,127 @@ namespace adrapi
 
 
             return ou;
+        }
+
+        private LdapAttributeSet GetAttributeSet(OU ou)
+        {
+            LdapAttributeSet attributeSet = new LdapAttributeSet();
+
+            attributeSet.Add(new LdapAttribute("objectclass", new string[] { "top", "organizationalUnit" }));
+            attributeSet.Add(new LdapAttribute("name", ou.Name));
+            attributeSet.Add(new LdapAttribute("ou", ou.Name));
+            attributeSet.Add(new LdapAttribute("description", ou.Description));
+            attributeSet.Add(new LdapAttribute("distinguishedName", ou.DN));
+
+
+            return attributeSet;
+        }
+
+        public int CreateOU(OU ou)
+        {
+
+            //Creates the List attributes of the entry and add them to attributeset
+
+            LdapAttributeSet attributeSet = GetAttributeSet(ou);
+
+            // DN of the entry to be added
+            string dn = ou.DN;
+
+            LdapEntry newEntry = new LdapEntry(dn, attributeSet);
+
+
+            var qMgmt = LdapQueryManager.Instance;
+
+            try
+            {
+                logger.Info("Saving ou={OU}", ou.DN);
+                qMgmt.AddEntry(newEntry);
+                return 0;
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error saving ou={DN}", ou.DN);
+                logger.Log(LogLevel.Error, ex);
+                return -1;
+            }
+
+        }
+
+        /// <summary>
+        /// Saves the OU.
+        /// </summary>
+        /// <returns>The OU. Must have DN set</returns>
+        /// <param name="ou">OU.</param>
+        public int SaveOU(OU ou)
+        {
+
+            var qMgmt = LdapQueryManager.Instance;
+
+            var modList = new List<LdapModification>();
+
+            var atributes = GetAttributeSet(ou);
+
+            //Get user from the Directory
+            try
+            {
+                var dou = GetOU(ou.DN);
+
+                var dattrs = GetAttributeSet(dou);
+
+
+                foreach (LdapAttribute attr in atributes)
+                {
+                    if (
+                        attr.Name != "ou"
+                        && attr.Name != "objectclass"
+                      )
+                    {
+
+                        var b1 = attr.ByteValue;
+
+                        var attribute = dattrs.GetAttribute(attr.Name);
+
+                        bool equal = true;
+
+                        if (attribute != null)
+                        {
+                            var b2 = attribute.ByteValue;
+
+                            equal = ByteTools.Equality(b1, b2);
+                        }
+
+
+                        if (!equal)
+                            modList.Add(new LdapModification(LdapModification.Replace, attr));
+                    }
+ 
+
+                }
+
+
+                try
+                {
+                    qMgmt.SaveEntry(ou.DN, modList.ToArray());
+                    return 0;
+
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("Error updating OU={DN}", ou.DN);
+                    logger.Log(LogLevel.Error, ex);
+                    return -1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error group not found");
+                logger.Log(LogLevel.Error, ex);
+                return -1;
+            }
+
+
         }
 
     }
--- domain/OU.cs
+++ domain/OU.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace adrapi.domain
 {
     public class OU
     {
-
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public string DN { get; set; }
