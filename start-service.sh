dbaa251c7ae2d4e2446295a8f5e42d366c0a28d9
Felipe Quintella <felipe@quintella.com>
2019-01-18 23:55:31 -0200

    INC

--- start-service.sh
+++ start-service.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-
 /opt/puppetlabs/puppet/bin/puppet apply  --modulepath=/etc/puppet/modules /etc/puppet/manifests/start.pp
 
 
 
+while [ ! -f /var/log/adrapi/internal-nlog.txt ]
+do
+  sleep 2
+done
+ls -l /var/log/adrapi/internal-nlog.txt
+
+tail -n 0 -f /var/log/adrapi/internal-nlog.txt &
+wait
