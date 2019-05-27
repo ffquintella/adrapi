2d554046c381a9040c2645a37d72d96825d7c2b1
Felipe F Quintella <felipe@quintella.com>
2019-05-27 20:07:49 -0300

    INC

--- adrapi/Tools/ListHelper.cs
+++ adrapi/Tools/ListHelper.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace adrapi.Tools
 {
-    public class ListHelper
+    public static class ListHelper
     {
-        
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T: ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
     }
 }
