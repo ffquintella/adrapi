5d904efa099444cadcb2f15b744045750cbc7c46
Felipe F Quintella <felipe@quintella.com>
2019-06-05 17:47:51 -0300

    INC

--- build/Build.cs
+++ build/Build.cs
@@ -23,7 +23,8 @@ class Build : NukeBuild
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    //readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    readonly Configuration Configuration = Configuration.Release;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
