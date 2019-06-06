81b9e096327113bd654a76009ca24aba3b2c42b7
Felipe F Quintella <felipe@quintella.com>
2019-06-06 18:46:15 -0300

    Configuring SSL certificate

--- adrapi/Program.cs
+++ adrapi/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -16,22 +17,29 @@ namespace adrapi
     {
         public static void Main(string[] args)
         {
+            #if DEBUG
             var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json", optional: false)
                 .AddCommandLine(args)
                 .Build();
+            #else
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddCommandLine(args)
+                .Build();
+            #endif
+            
 
-
-            var hostUrl = configuration["hosturl"];
+            /*var hostUrl = configuration["hosturl"];
             if (string.IsNullOrEmpty(hostUrl))
-                hostUrl = "http://0.0.0.0:5000";
+                hostUrl = "http://0.0.0.0:5000";*/
         
             // NLog: setup the logger first to catch all errors
             var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
             try
             {
                 logger.Debug("init main");
-                CreateWebHostBuilder(args)
-                    .UseUrls(hostUrl)
+                CreateWebHostBuilder(args, configuration)
                     .Build().Run();
             }
             catch (Exception ex)
@@ -48,14 +56,31 @@ namespace adrapi
 
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                    .ConfigureLogging(logging =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, IConfigurationRoot configuration)
+        {
+
+            string allowedHosts = configuration["AllowedHosts"] ?? "127.0.0.1";
+            if (allowedHosts == "*") allowedHosts = "0.0.0.0";
+            string certificateFile = configuration["certificate:file"] ?? "adrapi-dev.p12";
+            string certificatePassword = configuration["certificate:password"] ?? "adrapi-dev";
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
                     {
-                        logging.ClearProviders();
-                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                    })
-                    .UseNLog()
-                    .UseStartup<Startup>();
+                        options.Listen(IPAddress.Parse(allowedHosts), 5000);
+                        options.Listen(IPAddress.Parse(allowedHosts), 5001, listenOptions =>
+                            {
+                                listenOptions.UseHttps(certificateFile, certificatePassword);
+                            } );
+                    }
+                )
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
+                .UseStartup<Startup>();
+        }
     }
 }
